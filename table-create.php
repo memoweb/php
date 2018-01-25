821cff54cb1300d554c11773e1df8436d6ad5421
memoweb <mobilvadisi@gmail.com>
2018-01-26 00:20:40 +0300

    Add files via upload

---
+++ table-create.php
@@ -0,0 +1,22 @@
+<?php
+echo "<table border='2px'>";
+for ($satir=1;$satir<2;$satir++)
+{
+	echo "<tr>";
+	for($sutun=1;$sutun<2;$sutun++)
+	{
+		echo "\t <td>Ýsimler</td> \t <td>Soyisimler</td> <br />";
+	}
+	echo "</tr>";
+}
+
+for ($satir=1;$satir<2;$satir++)
+{
+	echo "<tr>";
+	for($sutun=1;$sutun<2;$sutun++)
+	{
+		echo "\t <td>Hakan</td> \t <td>ÇELÝKER</td> <br />";
+	}
+	echo "</tr>";
+}
+?>
